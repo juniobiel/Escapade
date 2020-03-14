ea978624e390c16a32f3da2dcf490b44295d1447
Gabriel Júnio <gabriel.junio@rocketmail.com>
2020-03-14 14:49:17 -0300

    Reverter commit e ajustes
    Ajustes feitos e voltei ao comit anterior ao merge da modelagem

--- Unity-Project-Escapade/Assets/Scripts/GameManager.cs
+++ Unity-Project-Escapade/Assets/Scripts/GameManager.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void LoadEscolaScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Escola", LoadSceneMode.Single);
     }
 }
--- Unity-Project-Escapade/Assets/Scripts/MenuPrincipal.cs
+++ Unity-Project-Escapade/Assets/Scripts/MenuPrincipal.cs
@@ -11,38 +11,56 @@ public class MenuPrincipal : MonoBehaviour
     string texto1, texto2, texto3;
 
     float contador, contadorAnim;
+
+    bool aumentar;
     // Start is called before the first frame update
     void Start()
     {
+        //inicializacao de variaveis
         contador = 0;
         contadorAnim = 0;
-        texto1 = startText.text;
+        aumentar = false;
+        //define texto
+        texto3 = startText.text;
         texto2 = "Pressione para iniciar..";
-        texto3 = "Pressione para iniciar.";
+        texto1 = "Pressione para iniciar.";
     }
 
     // Update is called once per frame
     void Update()
     {
+        //contador para animar o texto
         contadorAnim += Time.deltaTime;
+        //contador para liberar o start
         contador += Time.deltaTime;
-        if (contador >= 3f && Input.anyKeyDown)
+
+        AnimarTexto();
+
+        if (contador >= 1.5f && Input.anyKeyDown)
         {
             SceneManager.LoadScene("Escola", LoadSceneMode.Single);
         }
-        AnimarTexto();
+        
     }
 
     void AnimarTexto()
     {
         if (contadorAnim >= 0.5f)
-        {
-            if(startText.text.Equals(texto3))
-                startText.text = texto1;
-            else if(startText.text.Equals(texto1))
+        {            
+            if (startText.text.Equals(texto3))
+            {
                 startText.text = texto2;
-            else if(startText.text.Equals(texto2))
+                aumentar = false;
+            }
+            else if (startText.text.Equals(texto2) && aumentar == false)
+                startText.text = texto1;
+            else if (startText.text.Equals(texto2) && aumentar == true)
                 startText.text = texto3;
+            else if (startText.text.Equals(texto1))
+            {
+                startText.text = texto2;
+                aumentar = true;
+            }
             contadorAnim -= contadorAnim;
         }
     }
