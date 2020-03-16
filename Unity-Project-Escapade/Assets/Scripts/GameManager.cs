3fe39317ce0d6676eb718a1b0e3fba39e9ff890b
Gabriel Júnio <gabriel.junio@rocketmail.com>
2020-03-16 15:17:27 -0300

    HUD de objetivos
    Iniciei as configurações da HUD de objetivos e adição de motor de validação dos objetivos ainda não concluída.

--- Unity-Project-Escapade/Assets/Scripts/GameManager.cs
+++ Unity-Project-Escapade/Assets/Scripts/GameManager.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    Dictionary<int, string> missoes;
+
+    private void Start()
+    {
+        missoes = new Dictionary<int, string>();
+        missoes.Add(1, "Encontre a chave para sair da sala de limpeza");
+    }
     void LoadEscolaScene()
     {
         SceneManager.LoadScene("Escola", LoadSceneMode.Single);
